7d2365064fe580ac712dcef3b700f05c91838d7f
João Palmeiro <joaommpalmeiro@gmail.com>
2020-03-25 14:01:07 +0000

    Added: 2020-03-25 data.

--- dgs_concelhos.R
+++ dgs_concelhos.R
@@ -9,18 +9,21 @@ library(tidyverse)
 PDF_FOLDER_NAME <- "pdf"
 DATA_FOLDER_NAME <- "data"
 
-PDF_NAME <- "22_DGS_boletim_20200324_3.pdf"
-DATA_NAME <- "n_casos_concelho.csv"
+PDF_NAME <- "i026070.pdf"
+DATA_NAME <- paste(Sys.Date(), "csv", sep=".")
 
 PDF_PATH <- file.path(PDF_FOLDER_NAME, PDF_NAME)
 DATA_PATH <- file.path(DATA_FOLDER_NAME, DATA_NAME)
 
-BEGIN <- 8  
-FOOTNOTE <- 40
-
 doc <- str_squish(strsplit(pdf_text(PDF_PATH)[[3]], "\n")[[1]])
+doc
 
-doc <- doc[c(BEGIN:(FOOTNOTE-1), (FOOTNOTE+1):length(doc))]
+BEGIN <- 6
+FOOTNOTE <- 39
+DATE <-  42
+
+doc <- doc[c(BEGIN:(FOOTNOTE-1), (FOOTNOTE+1):(DATE-1))]
+doc
 
 doc_pre_table <- unlist(str_extract_all(doc, "([:alpha:][[:alpha:]\\s\\-()]+)\\s([[:digit:]]+)"))
 doc_pre_table
